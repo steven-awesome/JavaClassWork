6a58227afbad9665fe19ec8180a3950898f464bd
Steven Morrissey <steven@apicalservices.eu>
2016-02-25 17:31:45 -0500

    almost finished

--- Java2Class11FX/src/java2class11fx/Java2Class11FX.java
+++ Java2Class11FX/src/java2class11fx/Java2Class11FX.java
@@ -10,6 +10,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -21,20 +25,61 @@ public class Java2Class11FX extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        Button btn1 = new Button();
+        Button btn2 = new Button();
+        Button btn3 = new Button();
+        Button btn4 = new Button();
+        Label l1 = new Label();
+        Label l2 = new Label();
+        Label l3 = new Label();
+        TextField t1 = new TextField();
+        TextField t2 = new TextField();
+        TextField t3 = new TextField();
+        btn1.setText("Add");
+        btn2.setText("Subtract");
+        btn3.setText("Multiply");
+        btn4.setText("Divide");
+        HBox hbox1 = new HBox();
+        HBox hbox2 = new HBox();
+        
+        l1.setText("Number 1: ");
+        l2.setText("Number 2: ");
+        l3.setText("Number 3: ");
+        
+        GridPane pane = new GridPane();
+        pane.add(hbox1, 1, 0);
+        pane.add(hbox2, 1, 1);
+        
+        hbox1.getChildren().addAll(l1, t1, l2, t2, l3, t3);
+        hbox2.getChildren().addAll(btn1, btn2, btn3, btn4);
+        
+        btn1.setOnAction(event -> {
+            t3.setText(String.valueOf(Double.valueOf(t1.getText()) + Double.valueOf(t2.getText())));
+        });
+        
+        btn2.setOnAction(event -> {
+            t3.setText(String.valueOf(Double.valueOf(t1.getText()) - Double.valueOf(t2.getText())));
+        });
+        
+        btn3.setOnAction(event -> {
+            t3.setText(String.valueOf(Double.valueOf(t1.getText()) * Double.valueOf(t2.getText())));
+        });
+        
+        btn4.setOnAction(event -> {
+            t3.setText(String.valueOf(Double.valueOf(t1.getText()) / Double.valueOf(t2.getText())));
+        });
+        
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                t3.setText(String.valueOf(Double.valueOf(t1.getText()) + Double.valueOf(t2.getText())));
             }
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
         
-        Scene scene = new Scene(root, 300, 250);
+        
+        Scene scene = new Scene(pane, 300, 250);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
